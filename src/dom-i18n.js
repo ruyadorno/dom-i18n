52f0f78e52beaf6ab69cc41f81361254daf1bfc1
Ruy Adorno <ruyadorno@hotmail.com>
2015-03-28 20:01:04 -0400

    Added base source code

---
+++ src/dom-i18n.js
@@ -0,0 +1,59 @@
+(function (window) {
+
+  'use strict';
+
+  function getCurrentLanguage() {
+    var lang = window.navigator.languages ?
+      window.navigator.languages[0] :
+      (window.navigator.language || window.navigator.userLanguage);
+
+    return lang.indexOf('-') ? lang.split('-')[0] : lang;
+  }
+
+  function parseTranslatableStrings(options) {
+
+    var rootElement = options.rootElement || window.document;
+    var selector = options.selector || '[data-translatable]';
+    var separator = options.separator || ' // ';
+    var defaultLanguage = options.defaultLanguage || 'en';
+    var languages = options.languages || ['en'];
+    var translatableAttr = 'data-translatable-attr';
+    var currentLanguage = getCurrentLanguage();
+
+    function hasCurrentLanguage() {
+      return languages.indexOf(currentLanguage) > -1;
+    }
+
+    function getLanguageIndex() {
+      var index = languages.indexOf(currentLanguage);
+      return index > -1 ? index : languages.indexOf(defaultLanguage);
+    }
+
+    function translateString(value) {
+      return value.split(separator)[getLanguageIndex()];
+    }
+
+    function translateElement(elem) {
+      var attr = elem.getAttribute(translatableAttr);
+      if (attr) {
+        elem[attr] = translateString(elem[attr]);
+      } else {
+        elem.textContent = translateString(elem.textContent);
+      }
+    }
+
+    function translateElements() {
+      var elems = rootElement.querySelectorAll(selector);
+      for (var i = 0; i < elems.length; ++i) {
+        translateElement(elems[i]);
+      }
+    }
+
+    translateElements(selector);
+
+  }
+
+  window.parseTranslatableStrings = parseTranslatableStrings;
+
+})(window);
+
